d74c44e8688e89e39242f6623c26b6a0836f3569
ifgyong <fgyong214@gmail.com>
2020-08-10 10:57:50 +0800

    files

--- test/flutter_animations_test.dart
+++ test/flutter_animations_test.dart
@@ -1,7 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_animations/flutter_animations.dart';
-
 void main() {
 //  test('adds one to input values', () {
 //    final calculator = Calculator();
